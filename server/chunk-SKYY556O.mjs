44167aec80ed8cbd962249b5a8dfb677298a3c0e
v4vivekss <v4vivekss@gmail.com>
2024-02-04 23:38:49 +0100

    Auto-generated commit

---
+++ server/chunk-SKYY556O.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as r,b as i,c as n,d as p,e as m,l as f,n as s,o as a,r as c,x as d}from"./chunk-3WNF5FML.mjs";var l=(()=>{let o=class o{constructor(){this.title="fini"}};o.\u0275fac=function(e){return new(e||o)},o.\u0275cmp=r({type:o,selectors:[["app-root"]],standalone:!0,features:[m],decls:2,vars:0,template:function(e,y){e&1&&(i(0,"h1"),p(1,"Hello Vivo"),n())}});let t=o;return t})();var u=[];var v={providers:[d(u),a()]};var x={providers:[c()]},g=f(v,x);var h=()=>s(l,g),k=h;export{k as a};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-SKYY556O.mjs";import"./chunk-3WNF5FML.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
